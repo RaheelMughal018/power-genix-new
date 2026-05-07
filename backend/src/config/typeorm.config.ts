342c8f790f0bc294cb1a6145825cf2cb0a0a5710
Raheel Ahmad <raheelmughal018@gmail.com>
2026-05-07 20:27:17 +0500

    feat: add support for DATABASE_URL environment variable in TypeORM configuration

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -8,11 +8,15 @@ dotenv.config({ path: ENV ? `.env.${ENV}` : `.env` });
 
 const config = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST || 'localhost',
-  port: parseInt(process.env.DATABASE_PORT || '5432'),
-  username: process.env.DATABASE_USERNAME || 'postgres',
-  password: process.env.DATABASE_PASSWORD || '1234',
-  database: process.env.DATABASE_NAME || 'template',
+  ...(process.env.DATABASE_URL
+    ? { url: process.env.DATABASE_URL }
+    : {
+        host: process.env.DATABASE_HOST || 'localhost',
+        port: parseInt(process.env.DATABASE_PORT || '5432'),
+        username: process.env.DATABASE_USERNAME || 'postgres',
+        password: process.env.DATABASE_PASSWORD || '1234',
+        database: process.env.DATABASE_NAME || 'template',
+      }),
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
   autoLoadEntities: true,
